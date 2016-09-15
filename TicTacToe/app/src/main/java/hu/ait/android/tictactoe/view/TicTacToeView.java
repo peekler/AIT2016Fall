cf94534fc233d50085a1e52debc0b954f7ab534f
Peter Ekler <peter.ekler@aut.bme.hu>
2016-09-15 17:58:32 +0200

    ActivityLifeCycle Demo TicTacToe initial commit

---
+++ TicTacToe/app/src/main/java/hu/ait/android/tictactoe/view/TicTacToeView.java
@@ -0,0 +1,37 @@
+package hu.ait.android.tictactoe.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class TicTacToeView extends View {
+
+    private Paint paintBg;
+    private Paint paintLine;
+
+    public TicTacToeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        paintBg = new Paint();
+        paintBg.setColor(Color.BLACK);
+        paintBg.setStyle(Paint.Style.FILL);
+
+        paintLine = new Paint();
+        paintLine.setColor(Color.WHITE);
+        paintLine.setStyle(Paint.Style.STROKE);
+        paintLine.setStrokeWidth(5);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawRect(0, 0, getWidth(), getHeight(), paintBg);
+
+        canvas.drawLine(0, 0, getWidth(), getHeight(), paintLine);
+        canvas.drawCircle(200, 200, 100, paintLine);
+    }
+}
