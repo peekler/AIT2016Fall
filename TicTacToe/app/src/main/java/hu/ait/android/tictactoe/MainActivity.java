097353f414488ab843a4a8f83749a93b8b2afcd7
Peter Ekler <peter.ekler@aut.bme.hu>
2016-09-20 07:21:41 +0200

    Changes: ScrollView, Restart button, Touch events, Snackbar from view, draw game board, extracted strings, force to square size

--- TicTacToe/app/src/main/java/hu/ait/android/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/hu/ait/android/tictactoe/MainActivity.java
@@ -1,13 +1,52 @@
 package hu.ait.android.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import hu.ait.android.tictactoe.view.TicTacToeView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout layoutContent;
+    private TicTacToeView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutContent = (LinearLayout) findViewById(R.id.layoutContent);
+
+        gameView =
+                (TicTacToeView) findViewById(R.id.gameView);
+
+        Button btnRestart = (Button) findViewById(R.id.btnRestart);
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gameView.restartGame();
+            }
+        });
     }
+
+
+    public void showTooManyCirclesMessage() {
+        showSnackbarMessage(getString(R.string.text_too_many_circles));
+    }
+
+    private void showSnackbarMessage(String message) {
+        Snackbar.make(layoutContent, message, Snackbar.LENGTH_LONG).
+                setAction(R.string.action_clear, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        gameView.restartGame();
+                    }
+                }).show();
+    }
+
+
 }
--- TicTacToe/app/src/main/java/hu/ait/android/tictactoe/view/TicTacToeView.java
+++ TicTacToe/app/src/main/java/hu/ait/android/tictactoe/view/TicTacToeView.java
@@ -4,13 +4,23 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hu.ait.android.tictactoe.MainActivity;
 
 public class TicTacToeView extends View {
 
     private Paint paintBg;
     private Paint paintLine;
+
+    private List<PointF> circlesList = new ArrayList<>();
 
     public TicTacToeView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -31,7 +41,66 @@ public class TicTacToeView extends View {
 
         canvas.drawRect(0, 0, getWidth(), getHeight(), paintBg);
 
-        canvas.drawLine(0, 0, getWidth(), getHeight(), paintLine);
-        canvas.drawCircle(200, 200, 100, paintLine);
+        drawGameField(canvas);
+
+        drawCircles(canvas);
+
     }
+
+    private void drawGameField(Canvas canvas) {
+        // border
+        canvas.drawRect(0, 0, getWidth(), getHeight(), paintLine);
+        // two horizontal lines
+        canvas.drawLine(0, getHeight() / 3, getWidth(), getHeight() / 3,
+                paintLine);
+        canvas.drawLine(0, 2 * getHeight() / 3, getWidth(),
+                2 * getHeight() / 3, paintLine);
+
+        // two vertical lines
+        canvas.drawLine(getWidth() / 3, 0, getWidth() / 3, getHeight(),
+                paintLine);
+        canvas.drawLine(2 * getWidth() / 3, 0, 2 * getWidth() / 3, getHeight(),
+                paintLine);
+    }
+
+    private void drawCircles(Canvas canvas) {
+        for (PointF circleMid : circlesList) {
+            canvas.drawCircle(circleMid.x, circleMid.y,100, paintLine);
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.d("TAG_DRAW", "x: " + event.getX() + " # y: " + event.getY());
+            circlesList.add(new PointF(event.getX(),
+                    event.getY()));
+
+            // redraw this view
+            invalidate();
+
+            if (circlesList.size() > 5) {
+                ((MainActivity)getContext()).showTooManyCirclesMessage();
+            }
+
+        }
+
+        return true;
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int w = MeasureSpec.getSize(widthMeasureSpec);
+        int h = MeasureSpec.getSize(heightMeasureSpec);
+        int d = w == 0 ? h : h == 0 ? w : w < h ? w : h;
+        setMeasuredDimension(d, d);
+    }
+
+    public void restartGame() {
+        circlesList.clear();
+        invalidate();
+    }
+
 }
